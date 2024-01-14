3143b2787995bdd7eb8e0ce0160e2fb5a2bf11ee
Md Usman Ansari <md.usmanansari1999@gmail.com>
2024-01-14 19:55:22 +0530

    feat: improcve log and README

--- README.md
+++ README.md
@@ -1,2 +1,65 @@
-# multicmd-runner
-A CLI helper for run multiple commands at the same time.
+# ✨ multicmd-runner
+
+### A CLI helper for run multiple commands at the same time.
+
+### 🚀 Quickstart
+
+Install via NPM:
+
+```bash
+# Using Yarn:
+$ yarn global multicmd-runner
+
+# or, using NPM
+$ npm i -g multicmd-runner
+```
+
+`cd` into your root of project's directory, and try initialize the multicmd-runner
+
+```bash
+$ multicmd-runner init
+```
+
+Now in root directory there is created a file `multicmd-runner.config.json` Something look like this 👇🏻
+
+```json
+{
+  "dev": [
+    {
+      "title": "Dashboard Development Server",
+      "command": "pnpm dev",
+      "location": "/dashboard"
+    }
+  ],
+  "build": [
+    {
+      "title": "Dashboard Build",
+      "command": "pnpm build",
+      "location": "/dashboard"
+    }
+  ],
+  ...
+}
+```
+
+or you can create direclty array of commands objects.
+
+```json
+[
+  {
+    "title": "Dashboard Development Server",
+    "command": "pnpm dev",
+    "location": "/dashboard"
+  }
+]
+```
+
+Object must follows as this format 👇🏻
+
+```json
+{
+  "title": "Your title",
+  "command": "Specify command",
+  "location": "path where command is run."
+}
+```
--- src/utils.js
+++ src/utils.js
@@ -4,24 +4,50 @@ const file = require("./file.json");
 const CONFIG_FILENAME = "multicmd-runner.config.json";
 
 function createConfigFile() {
-  if (!fs.existsSync(CONFIG_FILENAME)) {
+  if (fs.existsSync(CONFIG_FILENAME)) {
+    console.info(`\n✨ ${CONFIG_FILENAME} is already exits.\n`);
+  } else {
     fs.writeFileSync(CONFIG_FILENAME, JSON.stringify(file), "utf8");
+    console.info(`\n🚀 ${CONFIG_FILENAME} is created.\n`);
   }
 }
 
 function isNoCommandExists(data, type) {
   if (!data && !type) {
-    console.error(chalk.red("Error: Config is empty."));
+    console.error(chalk.red("\n Error: Config is empty."));
     process.exit(0);
   } else if (!type && !Array.isArray(data)) {
     console.error(
       chalk.red(
-        "Error: If there is no category, JSON should be an array of objects."
+        "\n🐞 Error: If there is no specific key. JSON should be an array of commands."
       )
     );
+    console.info(chalk.bold(`\nExample:`));
+    console.info(`[{
+     "title": "Dashboard Development Server",
+     "command": "pnpm dev",
+     "location": "/dashboard"
+}]
+    `);
     process.exit(0);
   } else if (type && Array.isArray(data)) {
-    console.error(chalk.red("Error: JSON should be an array of objects"));
+    console.error(
+      chalk.red("🐞 Error: JSON should be an array of commands.\n")
+    );
+    console.info(chalk.bold(`Example:`));
+    console.info(`{
+      "${type}": [
+        {
+          "title": "Dashboard Development Server",
+          "command": "pnpm dev",
+          "location": "/dashboard"
+        }
+      ]
+}
+    `);
+    process.exit(0);
+  } else if (Array.isArray(data) && data.length === 0) {
+    console.info("\n😅 No commands specified!.");
     process.exit(0);
   }
   return type ? data[type] : data;
@@ -33,11 +59,15 @@ function readConfigFile(type) {
     const jsonData = JSON.parse(file);
     return isNoCommandExists(jsonData, type);
   } catch (error) {
-    console.error(
-      chalk.red(
-        "Error: 'multicmd-runner.config.json' file doesn't exist! Try 'multicmd-runner init' first.\n"
-      )
-    );
+    if (error.message === "Unexpected end of JSON input") {
+      console.error(chalk.red("\n🐞 Error: Not a valid json file."));
+    } else {
+      console.error(
+        chalk.red(
+          "\n🐞 Error: 'multicmd-runner.config.json' file doesn't exist! Try 'multicmd-runner init' first.\n"
+        )
+      );
+    }
     process.exit(0);
   }
 }
